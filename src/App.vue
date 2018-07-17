f9a298dedfe558a07a0a96be478780b432ebfd98
Chris D Brown <owner@chrisdbrown.co.uk>
2018-07-17 23:47:18 +0100

    feat: Make it look not terrible

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,25 @@
 <template>
   <div id="app">
-    <payment v-for="payment in payments" v-bind:key="payment.payment_id" v-bind:info="payment"></payment>
+    <section class="hero is-info">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            Live Venmo Payments
+          </h1>
+          <h2 class="subtitle">
+            Venmo's public feed gives away a surprising amount of information...
+          </h2>
+        </div>
+      </div>
+    </section>
+
+    <div class="columns">
+      <div class="column is-two-thirds is-offset-2">
+        <transition-group name="list-complete" class="div">
+          <payment v-for="payment in payments" v-bind:key="payment.payment_id" v-bind:info="payment"></payment>
+        </transition-group>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -44,8 +63,16 @@ export default {
       setTimeout(function(){
         self.poll();
         self.loop();
-      }, randomish() * 700);
+      }, randomish() * 1000);
     }
   }
 };
 </script>
+
+<style>
+  .list-complete-enter, .list-complete-leave-to
+  /* .list-complete-leave-active below version 2.1.8 */ {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+</style>
--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -1,13 +1,33 @@
 <template>
-  <div>
-    Sender: {{info.actor.name}} ({{info.actor.username}})<br />
-    Recipient: {{info.transactions[0].target.name}} ({{info.transactions[0].target.username}})<br />
-    Message: {{info.message}}<br />
+  <div class="card list-complete-item">
+    <div class="card-content">
+      <p class="title is-5">{{info.message}}</p>
+      <span class="is-pulled-right"><a :href="`https://venmo.com/${info.permalink}`">{{info.created_time | moment('HH:mm:ss')}}</a></span>
+    </div>
+    <footer class="card-footer">
+      <p class="card-footer-item">
+        <span>
+          From {{info.actor.name}} (<a :href="`https://venmo.com/${info.actor.username}`">@{{info.actor.username}}</a>)
+        </span>
+      </p>
+      <p class="card-footer-item">
+        <span>
+          To {{info.transactions[0].target.name}} (<a :href="`https://venmo.com/${info.transactions[0].target.name}`">@{{info.transactions[0].target.name}}</a>)
+        </span>
+      </p>
+    </footer>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['info']
+  props: ['info'],
 };
 </script>
+
+<style>
+  .list-complete-item {
+    transition: all 1s;
+    margin-top: 20px;
+  }
+</style>
